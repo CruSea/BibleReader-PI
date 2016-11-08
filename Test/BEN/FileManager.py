4472d885a94ec10648b37a6a5f33a1a99b47d8a2
BENGEOS <george.beng@gmail.com>
2016-11-08 16:24:04 +0300

    start file manager class

---
+++ Test/BEN/FileManager.py
@@ -0,0 +1,15 @@
+import os
+class FileManager(object):
+    def __init__(self,directory):
+        self.homeDir = directory
+        pass
+    def getAllFiles(self):
+        dirs = os.listdir(self.homeDir)
+        return dirs
+    def getAllFolders(self):
+        dirs = os.listdir(self.homeDir)
+        return dirs
+
+
+flm = FileManager("/");
+print flm.getAllFolders()
