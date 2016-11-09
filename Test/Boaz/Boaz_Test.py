46a2b1690dc3292e6965f07739ba852c7effb341
grace\Boaz <boazberhanu.jis@gmail.com>
2016-11-09 02:29:13 -0800

    test

--- Test/Boaz/Boaz_Test.py
+++ Test/Boaz/Boaz_Test.py
@@ -4,6 +4,7 @@ print x
 y = os.listdir(x) # in order to now what is in the current working dir and files in that dir
 print y
 os.rename('woman.txt', 'man.txt') # in order to rename a file in the current working dir
+print  "ji"
 
 
 
